aac04abcfae7d5590a65c60d1e5adbfbc824151a
Raposo67 <fernadoazevedofernandoserjo@gmail.com>
2024-02-10 18:47:18 +0000

    kkk

--- vscode.sh
+++ vscode.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-apt install ./code_1.85.1-1702461056_arm64.deb
-apt update
-clear
-rm -rf /usr/share/applications/code.desktop
-mv code.desktop /usr/share/applications/
-echo "..."
-sleep 2
-echo "procure na sua lista de aplicativos o Vscode"
-sleep 2
-clear
+apt install wget 
+
+
+wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/31c37ee8f63491495ac49e43b8544550fbae4533/code_1.86.1-1707296902_arm64.deb"
+apt install ./code_1.86.1-1707296902_arm64.deb
+if [ "$?" == 0 ]; then
+ echo "instalacao concluida"
+else
+ echo "ocorreu algum erro"
+fi
+echo "Corrigindo icompatibilidades"
+sed -i "s/--unity-launch %F/--no-sandbox/g" /usr/share/applications/code.desktop
+sed -i "s/--new-window %F/--no-sandbox/g" /usr/share/applications/code.desktop
+if [ "$?" == 0 ]; then
+ echo "concluido"
+ echo ""
+ echo "Vscode Instalado com Sucesso!!!"
+else
+ echo "ocorreu algum erro"
+ apt install sed
+ sed -i "s/--new-window %F/--no-sandbox/g" /usr/share/applications/code.desktop
+ sed -i "s/--unity-launch %F/--no-sandbox/g" /usr/share/applications/code.desktop
+ echo "Vscode instalado com sucesso!!!"
+fi
+

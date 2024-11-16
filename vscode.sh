72be2b24dee5cbb3f728db345b0347348511283d
Raposo57 <raposodocanal@gmail.com>
2024-11-16 01:17:33 +0000

    vscode

--- vscode.sh
+++ vscode.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
-apt install wget 
 
-
-wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/31c37ee8f63491495ac49e43b8544550fbae4533/code_1.86.1-1707296902_arm64.deb"
-apt install ./code_1.86.1-1707296902_arm64.deb
-if [ "$?" == 0 ]; then
- echo "instalacao concluida"
-else
- echo "ocorreu algum erro"
+################# INSTALAÇÃO ####################
+apt install wget -y
+clear
+sleep 2
+echo "OK"
+wget -O code_latest_arm64.deb https://update.code.visualstudio.com/latest/linux-deb-arm64/stable
+apt install ./code_latest_arm64.deb
+if [ "$?" -gt 0 ]; then
+ echo "ocorreu algum erro!"
+ exit
 fi
-echo "Corrigindo icompatibilidades"
-sed -i "s/--unity-launch %F/--no-sandbox/g" /usr/share/applications/code.desktop
+sudo apt-get install -f
+
+sed -i "s/ %F/ --no-sandbox/g" /usr/share/applications/code.desktop
+if [ "$?" -gt 0 ]; then
+ echo "ocorreu algum erro!"
+ exit
+else
+ clear
+ echo "espere um momento"
+ sleep 2
+fi
 sed -i "s/--new-window %F/--no-sandbox/g" /usr/share/applications/code.desktop
-if [ "$?" == 0 ]; then
- echo "concluido"
- echo ""
- echo "Vscode Instalado com Sucesso!!!"
+if [ "$?" -gt 0 ]; then
+ echo "ocorreu algum erro!"
+ exit
 else
- echo "ocorreu algum erro"
- apt install sed
- sed -i "s/--new-window %F/--no-sandbox/g" /usr/share/applications/code.desktop
- sed -i "s/--unity-launch %F/--no-sandbox/g" /usr/share/applications/code.desktop
- echo "Vscode instalado com sucesso!!!"
+ clear
+ echo "espere mais um pouco"
+ sleep 3
 fi
+clear
+echo "[+] VISUAL STUDIO CODE [+]"
+echo ""
+echo " PESQUISE/VEJA NA LISTA DE APLICATIVOS!"
+########################################################
 
+# É Como Um Sonho que Talvez eu nem tenha a Capacidadede conquistar!
